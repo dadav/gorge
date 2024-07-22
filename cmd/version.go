8bc8c8fb856a0e3923740f5a620ba9a1410be09b
dadav <33197631+dadav@users.noreply.github.com>
2024-07-22 18:12:06 +0200

    chore: version++

--- cmd/version.go
+++ cmd/version.go
@@ -22,7 +22,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version string = "0.5.0-alpha"
+var version string = "0.6.0-alpha"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
